735b694d0027ccb790c79645233ea706cfc92a4a
Gerardo Carlos Roderico Tan <gerardocarlosroderico.tan@qvcjp.com>
2019-11-26 15:54:10 +0900

    Setup project

---
+++ SwiftUI Practice/SwiftUI Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Practice
+//
+//  Created by Gerardo Carlos Roderico Tan on 2019/11/26.
+//  Copyright © 2019 nytfury47. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
