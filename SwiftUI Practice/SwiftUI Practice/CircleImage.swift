a99f5e997ebcb0a0134e7b99d33a27aba7059504
Gerardo Carlos Roderico Tan <gerardocarlosroderico.tan@qvcjp.com>
2019-11-26 17:40:15 +0900

    Essentials - Creating and Combining Views

---
+++ SwiftUI Practice/SwiftUI Practice/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUI Practice
+//
+//  Created by Gerardo Carlos Roderico Tan on 2019/11/26.
+//  Copyright © 2019 nytfury47. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ SwiftUI Practice/SwiftUI Practice/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  SwiftUI Practice
+//
+//  Created by Gerardo Carlos Roderico Tan on 2019/11/26.
+//  Copyright © 2019 nytfury47. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
